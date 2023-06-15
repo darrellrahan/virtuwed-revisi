23c7f7693d93dc9a9b9819cbab55edfc09205c61
hasnat5 <hasnatferdiananda5@gmail.com>
2023-06-15 23:37:23 +0700

    setup initial camera

--- src/components/Ballroom.tsx
+++ src/components/Ballroom.tsx
@@ -4,12 +4,21 @@ import * as THREE from 'three';
 
 
 //TODO:
-// 1. image setup
 // 2. image feature
 // 3. livestream feature
-// 4. storytelling
-// 5. gift menu
+// 4. infostop gift menu
+// 5. storytelling
+
+// minor feature
 // 6. leaderboard
+
+// ---feature needed
+// infospot hover element
+// loading progress
+// 3d model
+// storytelling
+
+
 
 const Ballroom = () => {
     const [isClient, setIsClient] = useState(false);
@@ -20,36 +29,60 @@ const Ballroom = () => {
 
     useEffect(() => {
         if (isClient) {
+            let pov = 'daridepan'
             const PANOLENS = require('panolens'); // Use require for server-side compatibility
+            const lookAtPositions = [
+                new THREE.Vector3(5000, 0, 0), //depan
+                new THREE.Vector3(5000, 0, 0), //mid
+                new THREE.Vector3(-5000, 0, 0) //lorong
+            ];
+
 
             const viewer = new PANOLENS.Viewer({ container: document.getElementById('ballroom'), enableReticle: false, output: 'console', viewIndicator: true });
 
 
+            //ASSET SETUP 
             const tengah = new PANOLENS.ImagePanorama('/assets/ballroom/tengah.jpg');
+            tengah.addEventListener('enter-fade-start', function () {
+                pov == 'daridepan' ?
+                    viewer.tweenControlCenter(lookAtPositions[1], 0)
+                    :
+                    viewer.tweenControlCenter(lookAtPositions[2], 0)
+
+            });
 
             const depan = new PANOLENS.ImagePanorama('/assets/ballroom/depan.jpg');
+            depan.addEventListener('enter-fade-start', function () {
+                viewer.tweenControlCenter(lookAtPositions[0], 0);
+            });
 
             const lorong = new PANOLENS.ImagePanorama('/assets/ballroom/lorong.jpg');
+            lorong.addEventListener('enter-fade-start', function () {
+                viewer.tweenControlCenter(lookAtPositions[2], 0);
+            });
 
 
+            // LINKING SETUP
             const infospotMid = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-            infospotMid.position.set(-100, -500, -5000);
+            infospotMid.position.set(5000, 0, 0);
             infospotMid.addHoverText("tengah");
 
             infospotMid.addEventListener('click', function () {
                 viewer.setPanorama(tengah);
+                pov = 'daridepan'
             });
 
             const infospotMid2 = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-            infospotMid2.position.set(-100, -500, 5000);
+            infospotMid2.position.set(-5000, 0, 0);
             infospotMid2.addHoverText("tengah");
 
             infospotMid2.addEventListener('click', function () {
                 viewer.setPanorama(tengah);
+                pov = 'daribelakang'
             });
 
             const infospotFront = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-            infospotFront.position.set(-100, -500, -5000);
+            infospotFront.position.set(5000, 0, 0);
             infospotFront.addHoverText("depan");
 
             infospotFront.addEventListener('click', function () {
@@ -57,7 +90,7 @@ const Ballroom = () => {
             });
 
             const infospotHallway = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
-            infospotHallway.position.set(-100, -500, 5000);
+            infospotHallway.position.set(-5000, 0, 0);
             infospotHallway.addHoverText("belakang");
 
             infospotHallway.addEventListener('click', function () {
