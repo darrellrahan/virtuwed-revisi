514d707f0927d7fcffa0054c19c9e9bd3091806d
hasnat5 <hasnatferdiananda5@gmail.com>
2023-09-05 17:53:59 +0700

    add hardcopy assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,22 +26,25 @@ module.exports = {
       },
       backgroundImage: {
         // LANDING PAGE PURPOSE
-        'tagline': "url(https://images.unsplash.com/photo-1533091090875-1ff4acc497dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80)",
-        'cta': "url(https://images.pexels.com/photos/56926/pexels-photo-56926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+        'tagline': "url(/assets/landingpage/tagline.avif)",
+        'cta': "url(/assets/landingpage/cta.webp)",
 
 
         // UNDANGAN DIGITAL PURPOSE
-        'flower-dark': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/flower-dark-min.png)",
-        'story-texture': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/orn-story-texture-min.png)",
-        'silent-garden': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/bg-savedate-min.png)",
-        'line-orn-event': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/line-orn-event-min.png)",
-        'forest': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/bg-forest-landscape-op5-min.png)",
-        'forest-potrait': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/bg-forest-potrait-min.png)",
-        'forest-landscape': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/bg-forest-landscape-min.png)",
-        'rsvp-texture': "url(https://katsudoto.id/media/template/custom/denis-serlyn/brown/bg-rsvp-texture-min.png)",
+        'flower-dark': "url(/assets/undanganDigital/website/flower-dark.png)",
+        'story-texture': "url(/assets/undanganDigital/website/story-texture.png)",
+        'silent-garden': "url(/assets/undanganDigital/website/silent-garden.png)",
+        'line-orn-event': "url(/assets/undanganDigital/website/line-orn-event.png)",
+        'forest': "url(/assets/undanganDigital/website/forest.png)",
+        'forest-potrait': "url(/assets/undanganDigital/website/forest-potrait.png)",
+        'forest-landscape': "url(/assets/undanganDigital/website/forest-landscape.png)",
+        'rsvp-texture': "url(/assets/undanganDigital/website/rsvp-texture.png)",
         'cover-wedding': "url(/assets/undanganDigital/Prewed24.jpeg)",
-        'cover-session': "url(https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80)",
-        'music-disc': "url(https://katsudoto.id/media/template/details/music.png)"
+        'cover-session': "url(/assets/undanganDigital/website/cover-session.avif)",
+        'music-disc': "url(/assets/undanganDigital/website/music-disc.png)",
+
+        // LOGIN & REGISTER
+        'register': "url(/assets/landingpage/register.avif)",
 
 
       },
@@ -53,5 +56,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 }
